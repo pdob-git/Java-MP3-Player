eb71c0c0ffbbe82f50f3be29a20fb6c7658ca4f9
Pawel <synk@poczta.onet.pl>
2021-05-22 01:19:16 +0200

    Icons have been added to play buttons
    Icons have been added to play buttons.
    Icons folder have been added to resources.
    Modifications:
    	modified:       src/main/java/org/player/mp3player/controllers/MainWindow.java
    	new file:       src/main/resources/org/player/mp3player/icons/eject.png
    	new file:       src/main/resources/org/player/mp3player/icons/next.png
    	new file:       src/main/resources/org/player/mp3player/icons/pause.png
    	new file:       src/main/resources/org/player/mp3player/icons/play.png
    	new file:       src/main/resources/org/player/mp3player/icons/previous.png
    	new file:       src/main/resources/org/player/mp3player/icons/stop.png

--- src/main/java/org/player/mp3player/controllers/MainWindow.java
+++ src/main/java/org/player/mp3player/controllers/MainWindow.java
@@ -7,9 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
 
 import java.io.File;
 import java.net.URL;
@@ -54,8 +58,23 @@ public class MainWindow implements Initializable {
     private TimerTask task;
     private boolean running;
 
+    private Image imagePrev;
+    private ImageView imageViewPrev;
+    private Image imagePlay;
+    private ImageView imageViewPlay;
+    private Image imageNext;
+    private ImageView imageViewNext;
+    private Image imagePause;
+    private ImageView imageViewPause;
+    private Image imageStop;
+    private ImageView imageViewStop;
+    private Image imageOpen;
+    private ImageView imageViewOpen;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        initButtons();
 
         songs = new ArrayList<File>();
         directory = new File("music");
@@ -119,5 +138,75 @@ public class MainWindow implements Initializable {
     }
 
     public void openPlayList(ActionEvent event) {
+    }
+
+    private void initButtons(){
+        //Set Prev Button Icon
+        imagePrev = new Image(this.getClass().getResourceAsStream("../icons/previous.png"));
+        imageViewPrev = new ImageView(imagePrev);
+        imageViewPrev.setFitHeight(20);
+        imageViewPrev.setPreserveRatio(true);
+        previousButton.setGraphic(imageViewPrev);
+        previousButton.setText(null);
+        Tooltip previousTooltip = new Tooltip("Previous");
+        previousTooltip.setFont(Font.font(9));
+        previousButton.setTooltip(previousTooltip);
+
+        //Set Play Button Icon
+        imagePlay = new Image(this.getClass().getResourceAsStream("../icons/play.png"));
+        imageViewPlay = new ImageView(imagePlay);
+        imageViewPlay.setFitHeight(20);
+        imageViewPlay.setPreserveRatio(true);
+        playButton.setGraphic(imageViewPlay);
+        playButton.setText(null);
+        Tooltip playTooltip = new Tooltip("Play");
+        playTooltip.setFont(Font.font(9));
+        playButton.setTooltip(playTooltip);
+
+        //Set Pause Button Icon
+        imagePause = new Image(this.getClass().getResourceAsStream("../icons/pause.png"));
+        imageViewPause = new ImageView(imagePause);
+        imageViewPause.setFitHeight(20);
+        imageViewPause.setPreserveRatio(true);
+        pauseButton.setGraphic(imageViewPause);
+        pauseButton.setText(null);
+        Tooltip pauseTooltip = new Tooltip("Pause");
+        pauseTooltip.setFont(Font.font(9));
+        pauseButton.setTooltip(pauseTooltip);
+
+
+        //Set Stop Button Icon
+        imageStop = new Image(this.getClass().getResourceAsStream("../icons/stop.png"));
+        imageViewStop = new ImageView(imageStop);
+        imageViewStop.setFitHeight(20);
+        imageViewStop.setPreserveRatio(true);
+        stopButton.setGraphic(imageViewStop);
+        stopButton.setText(null);
+        Tooltip stopTooltip = new Tooltip("Stop");
+        stopTooltip.setFont(Font.font(9));
+        stopButton.setTooltip(stopTooltip);
+
+        //Set Next Button Icon
+        imageNext = new Image(this.getClass().getResourceAsStream("../icons/next.png"));
+        imageViewNext = new ImageView(imageNext);
+        imageViewNext.setFitHeight(20);
+        imageViewNext.setPreserveRatio(true);
+        nextButton.setGraphic(imageViewNext);
+        nextButton.setText(null);
+        Tooltip nextTooltip = new Tooltip("Next");
+        nextTooltip.setFont(Font.font(9));
+        nextButton.setTooltip(nextTooltip);
+
+        //Set Open Button Icon
+        imageOpen = new Image(this.getClass().getResourceAsStream("../icons/eject.png"));
+        imageViewOpen = new ImageView(imageOpen);
+        imageViewOpen.setFitHeight(20);
+        imageViewOpen.setPreserveRatio(true);
+        openButton.setGraphic(imageViewOpen);
+        openButton.setText(null);
+        Tooltip openTooltip = new Tooltip("Open");
+        openTooltip.setFont(Font.font(9));
+        openButton.setTooltip(openTooltip);
+
     }
 }
